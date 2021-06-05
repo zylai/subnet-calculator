4322cf103d413ba3365b812e41afe408572892eb
zylai <40312846+zylai@users.noreply.github.com>
2021-06-05 15:40:14 -0700

    Update practice.sh

--- practice.sh
+++ practice.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+RED_BOLD='\033[1;31m'
+GREEN_BOLD='\033[1;32m'
+NORMAL='\033[0m'
+
 # Generate a random IP, first octet cannot be 0
 # https://blog.eduonix.com/shell-scripting/generating-random-numbers-in-linux-shell-scripting/
 ip=`echo "$(($(($RANDOM%254))+1)).$((RANDOM%255)).$((RANDOM%255)).$((RANDOM%255))"`
@@ -16,6 +20,8 @@ printf "> "
 
 read response
 
+echo "--------------------------------------------------"
+
 response_network=`echo $response | cut -f1 -d/`
 response_first=`echo $response | cut -f2 -d/`
 response_last=`echo $response | cut -f3 -d/`
@@ -28,12 +34,14 @@ answer_first=`echo $answer | cut -f3 -d:`
 answer_last=`echo $answer | cut -f4 -d:`
 answer_broadcast=`echo $answer | cut -f5 -d:`
 
-echo $response_network
-echo $response_first
-echo $response_last
-echo $response_broadcast
+for address in network first last broadcast
+do
+	response_address="response_$address"
+	answer_address="answer_$address"
 
-echo $answer_network
-echo $answer_first
-echo $answer_last
-echo $answer_broadcast
+	if [ "${!answer_address}" = "${!response_address}" ]; then
+		echo -e "$address: ${!answer_address} — ${GREEN_BOLD}CORRECT${NORMAL}"
+	else
+		echo -e "$address: ${!answer_address} — ${RED_BOLD}INCORRECT${NORMAL}"
+	fi
+done
